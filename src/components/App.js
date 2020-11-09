1b60684a113303c704536d073601952ba100071c
darshanshah1921999 <54939839+darshanshah1921999@users.noreply.github.com>
2020-11-09 10:50:15 +0530

    update in list

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,16 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  list = [1, 2, 3, 4, 5];
   render() {
+    let relatives = ["Darshan", "Prashil", "Prerak", "Jimit", "Shubham"];
     return (
       <div id="main">
-        <ul>
-          {this.list.map((item) => (
-            <li key={item}>{item}</li>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={"relativeListItem" + (index + 1)}>{relative}</li>
           ))}
-        </ul>
+        </ol>
+        }
       </div>
     );
   }
